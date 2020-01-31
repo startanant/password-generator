6b54472129b7d91cd759911d4963aa173d287c67
Anant S <startanant@gmail.com>
2020-01-31 11:24:39 -0500

    Add working password generator for upper case & lower case character types

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,36 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+lower = "abcdefghijklmnopqrstuvwxyz";
+upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+charset = "";
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  if (isNaN (passwordLength.value)){
+    alert('Enter a number between 8 and 128');
+  } else if (passwordLength.value > 128 || passwordLength.value < 8) {
+    alert('Enter a number between 8 and 128');
+  } else {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+    charset = "";
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Generate password
+
+function generatePassword() {
+  var isUpperChecked = $("#upperCheck").is(':checked');
+  var isLowerChecked = $("#lowerCheck").is(':checked');
+  if(isUpperChecked) {
+    charset = charset.concat(upper)
+  }
+  if(isLowerChecked) {
+    charset = charset.concat(lower)
+  }
+  //alert(charset);
+  var result = "";
+	for (var i = 0; i < passwordLength.value; i++)
+		result += charset[Math.floor(Math.random() * charset.length)];
+	return result;
+  
+}
