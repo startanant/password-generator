e51d2ebe628b5fbb92a783f626f5720bca36947e
Anant S <startanant@gmail.com>
2020-02-03 09:56:54 -0500

    Validate the generated password & shuffle

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
+//Character sets
 lower = "abcdefghijklmnopqrstuvwxyz";
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 special = "!@#";
-charset = "";
 
 // Write password to the #password input
 function writePassword() {
@@ -11,35 +11,68 @@ function writePassword() {
     alert('Enter a number between 8 and 128');
   } else {
     var password = generatePassword();
+    //alert(password);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
-    charset = "";
   }
 }
 
 //Generate password
 
 function generatePassword() {
+  result = "";
   var isUpperChecked = $("#upperCheck").is(':checked');
   var isLowerChecked = $("#lowerCheck").is(':checked');
   var isSpecialChecked = $("#specialCheck").is(':checked');
-  if(isUpperChecked) {
-    charset = charset.concat(upper)
+  var checked_boxes =  $(':checkbox:checked').length;
+  
+  //Make sure there is atleast one of each type
+  for (var i = 0; i < checked_boxes.value; i++) {
+    if(isUpperChecked) {
+      result += upper[Math.floor(Math.random() * upper.length)];
+    }
+    if(isLowerChecked) {
+      result += lower[Math.floor(Math.random() * lower.length)];
+    }
+    if(isSpecialChecked) {
+      result += special[Math.floor(Math.random() * special.length)];
+    }
   }
-  if(isLowerChecked) {
-    charset = charset.concat(lower)
+
+  //Fill the remaining slots
+  while(result.length <= passwordLength.value) {
+    if(isUpperChecked) {
+      result += upper[Math.floor(Math.random() * upper.length)];
+    }
+    if(isLowerChecked) {
+      result += lower[Math.floor(Math.random() * lower.length)];
+    }
+    if(isSpecialChecked) {
+      result += special[Math.floor(Math.random() * special.length)];
+    }
   }
-  if(isSpecialChecked) {
-    charset = charset.concat(special)
-  }
-  alert(charset);
-  var result = "";
-	result = buildResult(result, charset);
-	return result;
+
+  //Shuffle
+  result_array = result.split("");
+  shuffled_result = shuffle(result_array);
+  return shuffled_result.join("");
 }
 
-function buildResult(result, charset) {
-  for (var i = 0; i < passwordLength.value; i++)
-    result += charset[Math.floor(Math.random() * charset.length)];
-  return result;
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
 }
