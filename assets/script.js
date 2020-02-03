e950f1d5c8a4f290f4d00d536568a35d6b1d4c62
Anant S <startanant@gmail.com>
2020-02-03 13:47:06 -0500

    Use switch statement for readability + fix length bug

--- assets/script.js
+++ assets/script.js
@@ -1,56 +1,78 @@
 //Character sets
 lower = "abcdefghijklmnopqrstuvwxyz";
 upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-special = "!@#";
+numbers = "1234567890";
+special = "#$%&()*+-";
+
 
 // Write password to the #password input
 function writePassword() {
-  if (isNaN (passwordLength.value)){
+  var passwordLength = $("#passwordLength").val();
+  if (isNaN (passwordLength)){
     alert('Enter a number between 8 and 128');
-  } else if (passwordLength.value > 128 || passwordLength.value < 8) {
+  } else if (passwordLength > 128 || passwordLength < 8) {
     alert('Enter a number between 8 and 128');
   } else {
-    var password = generatePassword();
-    //alert(password);
+    var password = generatePassword(passwordLength);
     var passwordText = document.querySelector("#password");
     passwordText.value = password;
   }
 }
 
 //Generate password
-
-function generatePassword() {
+function generatePassword(passwordLength) {
   result = "";
   var isUpperChecked = $("#upperCheck").is(':checked');
   var isLowerChecked = $("#lowerCheck").is(':checked');
   var isSpecialChecked = $("#specialCheck").is(':checked');
+  var isNumbersChecked = $("#numbersCheck").is(':checked');
+  
+  var charset_array = [];
   var checked_boxes =  $(':checkbox:checked').length;
   
   //Make sure there is atleast one of each type
-  for (var i = 0; i < checked_boxes.value; i++) {
-    if(isUpperChecked) {
-      result += upper[Math.floor(Math.random() * upper.length)];
-    }
-    if(isLowerChecked) {
-      result += lower[Math.floor(Math.random() * lower.length)];
-    }
-    if(isSpecialChecked) {
-      result += special[Math.floor(Math.random() * special.length)];
+  if(isUpperChecked) {
+    result += upper[Math.floor(Math.random() * upper.length)];
+    charset_array.push('upper');
+  }
+  if(isLowerChecked) {
+    result += lower[Math.floor(Math.random() * lower.length)];
+    charset_array.push('lower');
+  }
+  if(isSpecialChecked) {
+    result += special[Math.floor(Math.random() * special.length)];
+    charset_array.push('special');
+  }
+  if(isNumbersChecked) {
+    result += numbers[Math.floor(Math.random() * numbers.length)];
+    charset_array.push('numbers');
+  }
+  
+  remaining_characters = passwordLength - result.length;
+  
+  
+  //Fill the remaining slots
+  for(i = 0; i < remaining_characters; i++){
+    random_charset = Math.floor(Math.random() * checked_boxes);
+    switch(charset_array[random_charset]) {
+      case 'upper':
+        result += upper[Math.floor(Math.random() * upper.length)];
+        break;
+      case 'lower':
+        result += lower[Math.floor(Math.random() * lower.length)];
+        break;
+      case 'special':
+        result += special[Math.floor(Math.random() * special.length)];
+        break;
+      case 'numbers':
+        result += numbers[Math.floor(Math.random() * numbers.length)];
+        break;
+      default:
+        break;
     }
   }
 
-  //Fill the remaining slots
-  while(result.length <= passwordLength.value) {
-    if(isUpperChecked) {
-      result += upper[Math.floor(Math.random() * upper.length)];
-    }
-    if(isLowerChecked) {
-      result += lower[Math.floor(Math.random() * lower.length)];
-    }
-    if(isSpecialChecked) {
-      result += special[Math.floor(Math.random() * special.length)];
-    }
-  }
+  
 
   //Shuffle
   result_array = result.split("");
